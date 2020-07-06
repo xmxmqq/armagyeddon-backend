939c42cac3cc6b9b761e40cacd9c400805551b33
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-07-06 14:51:48 +0900

    change index icon

--- src/main/java/com/blockchain/armagyeddon/config/SecurityConfig.java
+++ src/main/java/com/blockchain/armagyeddon/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.blockchain.armagyeddon.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import lombok.RequiredArgsConstructor;
 
 
@@ -35,6 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/");
 
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        // 스태틱 리소스들은 시큐리티 적용 x
+    }
+
 
 }
 
