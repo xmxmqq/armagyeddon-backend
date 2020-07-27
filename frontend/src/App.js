2d66d194dc3904815faa07426a1e50682d7778fb
조승현 <49122299+wjrmffldrhrl@users.noreply.github.com>
2020-07-27 16:43:44 +0900

    react api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-function App() {
-  return (
-    
-        <h1>
-          Learn React
-        </h1>
+class App extends React.Component {
+  
+  
+  constructor(props){
+    super(props);
+    this.state = {
+        apisay: null,
+    };
+  }
+
+  componentDidMount(){
+      fetch("/react/test")
+      .then(res => res.json())
+      .then(
+          (result) => {
+              this.setState({
+                  apisay: result.springboot,
+                  
+              });
+          },
+
+         
+      )
+  }
+
+  render(){
+      const {apisay} = this.state;
+
       
-  );
+      return (
+          
+
+          <div>
+            <h1>
+              Learn React
+            </h1>
+            {apisay}
+          </div>
+      );
+  }
+
+ 
 }
 
 export default App;
