55e1987955f7a87d11bf29a13bb64422430ec798
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-03 00:42:05 +0900

    git ignore

--- src/main/java/com/blockchain/armagyeddon/login/WebMvcConfig.java
+++ src/main/java/com/blockchain/armagyeddon/login/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.blockchain.armagyeddon.login;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
