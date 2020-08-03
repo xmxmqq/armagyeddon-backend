4a9f45614b37dd88a74fe1d90741d33552240d0f
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-03 13:38:31 +0900

    index

--- frontend/src/Component/Routes.js
+++ frontend/src/Component/Routes.js
@@ -4,11 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "../Pages/Login";
 import SpiderMan from "../Pages/SpiderMan";
 import Header from "./Header";
-
+import Index from "../Pages/Index";
 
 export default () => (
     <Router>
         <Header/>
+        <Route path="/index" component={Index}/>
         <Route path="/login" component={Login}/>
         <Route path="/spiderman" component={SpiderMan}/>
     </Router>
